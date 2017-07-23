f194a68b757823bb4f0efffe66670db6ca7b9461
Eugene <rizr91@gmail.com>
2017-07-23 23:14:58 +0300

    header

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-action>
+            View Meetups
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon class="hidden-sm-and-up" @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat><v-icon dark left>supervisor_account</v-icon>View Meetups</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        sideNav: false,
+      };
+    },
+  };
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
